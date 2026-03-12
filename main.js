edaf18bbacd8166046d4bb7905de41e54fc6fc29
Patricia Martínez Cruz <patricia.martinez.cruz.pmn@gmail.com>
2026-03-12 11:54:38 +0100

    clean files + html showing objects

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-import { writeFile } from "fs/promises";
 import { loadPokemons } from "./pokemons.js";
 
-const pokemons = await loadPokemons(3);
-console.log(pokemons);
-// const html = render(pokemons);
-// await writeFile('index.html', html);
+const pokemons = await loadPokemons(2);
+const container = document.getElementById("pokemons");
 
+pokemons.forEach((pokemon) => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <p>ID: ${pokemon.id}</p>
+    <p>Name: ${pokemon.name}</p>
+    <p>Weight: ${pokemon.weight}</p>
+    <p>Height: ${pokemon.height}</p>
+  `;
+  container.appendChild(div);
+});
