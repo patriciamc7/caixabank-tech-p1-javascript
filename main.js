e207a5a3b26f55aad3c1b329520bc9a76771eb05
Patricia Martínez Cruz <patricia.martinez.cruz.pmn@gmail.com>
2026-03-12 12:27:23 +0100

    html button to show all objects

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ import { loadPokemons } from "./pokemons.js";
 
 const pokemons = await loadPokemons(2);
 const container = document.getElementById("pokemons");
+const input = document.getElementById("numPokemons");
+const button = document.getElementById("btnLoad");
 
-pokemons.forEach((pokemon) => {
-  const div = document.createElement("div");
-  div.innerHTML = `
-    <p>ID: ${pokemon.id}</p>
+button.addEventListener("click", async () => {
+  const numPokemons = parseInt(input.value);
+  if (isNaN(numPokemons)) {
+    alert("Please enter a valid number.");
+    return;
+  }
+
+  const pokemons = await loadPokemons(numPokemons);
+  container.innerHTML = "";
+
+  pokemons.forEach((pokemon) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <p>ID: ${pokemon.id}</p>
     <p>Name: ${pokemon.name}</p>
     <p>Weight: ${pokemon.weight}</p>
     <p>Height: ${pokemon.height}</p>
   `;
   container.appendChild(div);
+  });
 });
