f9fa5a5ec6ad3047ab6b000075b9a468f81980de
Patricia Martínez Cruz <patricia.martinez.cruz.pmn@gmail.com>
2026-03-12 20:14:09 +0100

    styles pokemon

--- pokemon/pokemoninfo.js
+++ pokemon/pokemoninfo.js
@@ -1,15 +1,15 @@
 import { loadPokemonInfo } from "./pokemons.js";
 
 const container = document.getElementById("pokemonInfo");
-console.log("window.location.href:", window.location.href);
-console.log("window.location.search:", window.location.search);
+
 const p = await loadPokemonInfo(
   new URLSearchParams(window.location.search).get("id"),
 );
 
 const pokemon = p[0];
 container.innerHTML = `
-  <h2>Name: ${pokemon.name} (ID: ${pokemon.id})</h2>
+  <h2>${pokemon.name}</h2>
+  <p>ID: ${pokemon.id}</p>
   <p>Weight: ${pokemon.weight}</p>
   <p>Height: ${pokemon.height}</p>
 `;
